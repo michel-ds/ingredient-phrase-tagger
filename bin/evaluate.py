e162d485983d657d779872a5c6abccb252e5a88f
Michel S <64908337+michel-ds@users.noreply.github.com>
2020-05-11 12:51:18 +0100

    Converted prints to Python 3.

--- bin/evaluate.py
+++ bin/evaluate.py
@@ -44,14 +44,13 @@ for sentence in sentences:
     if total_words_per_sentence == correct_words_per_sentence:
         correct_sentences += 1
 
-print
-print 'Sentence-Level Stats:'
-print '\tcorrect: ', correct_sentences
-print '\ttotal: ', total_sentences
-print '\t% correct: ', 100 * (correct_sentences / float(total_sentences))
 
-print
-print 'Word-Level Stats:'
-print '\tcorrect:', correct_words
-print '\ttotal:', total_words
-print '\t% correct:', 100 * (correct_words / float(total_words))
+print ('Sentence-Level Stats:')
+print ('\tcorrect: ', correct_sentences)
+print ('\ttotal: ', total_sentences)
+print ('\t% correct: ', 100 * (correct_sentences / float(total_sentences)))
+
+print ('Word-Level Stats:')
+print ('\tcorrect:', correct_words)
+print ('\ttotal:', total_words)
+print ('\t% correct:', 100 * (correct_words / float(total_words)))
